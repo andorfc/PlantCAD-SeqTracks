699facfb3703e12cc1d68017621a977e95da7ebd
Carson Andorf <andorfc@iastate.edu>
2025-05-23 16:52:52 -0500

    Delete my_bed.sh

--- my_bed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INPUT="./data/annotation.gff3"
-OUTPUT="./bed/"
-
-mkdir -p bed
-
-python ./python/generate_bed_files.py --gff $INPUT --output_dir $OUTPUT --genes_per_file 200 --padding 1000
